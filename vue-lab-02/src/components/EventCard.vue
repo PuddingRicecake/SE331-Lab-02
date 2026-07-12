9e91c4b8ae8b4e28b07a55f108b1a50006ac4e9f
Nattamon Tarnpipitchai <nattamon@Nattamons-MacBook-Pro.local>
2026-07-12 15:00:46 +0700

    Q5

--- vue-lab-02/src/components/EventCard.vue
+++ vue-lab-02/src/components/EventCard.vue
@@ -5,3 +5,22 @@
   <div class="greetings">
   </div>
 </template>
+
+<style scoped>
+.event-class {
+  display: flex;
+  justify-content: center;
+}
+
+.event-class {
+  padding: 20px;
+  width: 250px;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+}
+
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+</style>
