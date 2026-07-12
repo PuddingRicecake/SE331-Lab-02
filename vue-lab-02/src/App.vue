a4e11ad56d177a1afbc266f97a5061c553bbf59c
Nattamon Tarnpipitchai <nattamon@Nattamons-MacBook-Pro.local>
2026-07-12 22:48:51 +0700

    Q13

--- vue-lab-02/src/App.vue
+++ vue-lab-02/src/App.vue
@@ -9,7 +9,8 @@ import { RouterLink, RouterView } from 'vue-router'
 
       <div class="wrapper">
         <nav>
-          <RouterLink to="/">Home</RouterLink>
+          <RouterLink to="/">Home</RouterLink> |
+          <RouterLink to="/">Event</RouterLink> |
           <RouterLink to="/about">About</RouterLink>
         </nav>
       </div>
