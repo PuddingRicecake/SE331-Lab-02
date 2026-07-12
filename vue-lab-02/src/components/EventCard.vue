79b4a1283b07ee7d1ab7ac03970480bd86f8cf50
Nattamon Tarnpipitchai <nattamon@Nattamons-MacBook-Pro.local>
2026-07-12 22:05:57 +0700

    Q8.4

--- vue-lab-02/src/components/EventCard.vue
+++ vue-lab-02/src/components/EventCard.vue
@@ -1,17 +1,21 @@
 <script setup lang="ts">
   import { ref } from 'vue'
+  import type { Event } from '@/types'
+  defineProps<{
+    event: Event
+  }>()
 
-  const event = ref({
-    id: 5928101,
-    category: 'animal welfare',
-    title: 'Cat Adoption Day',
-    description: 'Find your new feline friend at this event.',
-    location: 'Meow Town',
-    date: 'January 28, 2026',
-    time: '12:00',
-    petAllowed: true,
-    organizer: 'Kat Laydee'
-  })
+  // const event = ref({
+  //   id: 5928101,
+  //   category: 'animal welfare',
+  //   title: 'Cat Adoption Day',
+  //   description: 'Find your new feline friend at this event.',
+  //   location: 'Meow Town',
+  //   date: 'January 28, 2026',
+  //   time: '12:00',
+  //   petAllowed: true,
+  //   organizer: 'Kat Laydee'
+  // })
 </script>
 
 <template>
